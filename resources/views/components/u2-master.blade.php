b78dc9df1d31533c4dfba6f003c9a753a4010490
Mykola Havrysh <decrier@ukr.net>
2025-04-01 15:03:24 +0200

    second commit

---
+++ resources/views/components/u2-master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Das wrapper-Template</title>
+</head>
+<body>
+    <nav>
+        <a href="portfolio">Portfolio</a>
+        <a href="service">Service</a>
+
+    </nav>
+
+    <h1>Laravel Bootstrap-Card</h1>
+    <div class="container">
+        @section('content')
+            <h1>Komponenten (moderne Variante)</h1>
+            <div class="card">
+                <div class="card-header">
+                    <h2>Gerüst</h2>
+                </div>
+
+                <div class="card-body">
+                    <h5 class="card-title">{{ $title }}</h5>
+                    <p class="card-text">{{ $slot }}</p>
+                </div>
+
+                <div class="card-footer">
+                    <p>{{ $footer}}</p>
+                </div>
+            </div>
+        @show
+    </div>
+
+</body>
+</html>
